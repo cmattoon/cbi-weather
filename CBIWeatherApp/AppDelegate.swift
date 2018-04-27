05a3b8378b273445f17959fc6382174a27cbf866
Curtis Mattoon <cmattoon@cmattoon.com>
2018-04-26 20:02:33 -0400

    Updates

--- CBIWeatherApp/AppDelegate.swift
+++ CBIWeatherApp/AppDelegate.swift
@@ -7,10 +7,28 @@
 //
 
 import Cocoa
+import Foundation
 
+extension Date {
+    func dateAt(hours: Int, minutes: Int) -> Date {
+        let calendar = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)!
+        var date_components = calendar.components(
+            [NSCalendar.Unit.year,
+             NSCalendar.Unit.month,
+             NSCalendar.Unit.day],
+            from: self)
+        date_components.hour = hours
+        date_components.minute = minutes
+        date_components.second = 0
+        let newDate = calendar.date(from: date_components)!
+        return newDate
+    }
+}
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     let TIMER_INTERVAL = 60.0 * 5
+    
+    let SUNSET_API = "https://api.sunrise-sunset.org/json?lat=42.3599&lng=-71.0730&date=today"
     
     let ENDPOINT = "https://portal2.community-boating.org/pls/apex/CBI_PROD.FLAG_JS"
     
@@ -22,7 +40,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to initialize your application
         if let button = statusItem.button {
             button.image = NSImage(named:NSImage.Name("BlackFlag"))
-            button.action = #selector(getWeather(_:))
+            button.action = #selector(AppDelegate.updateWeather)
         }
         timer = Timer.scheduledTimer(timeInterval: TIMER_INTERVAL,
                                      target: self,
@@ -37,13 +55,39 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
     
-    func withinNormalHours() -> Bool {
-            return true
+    func constructMenu() {
+        let menu = NSMenu()
+        let sunset_time = "XX:XX"
+        menu.addItem(NSMenuItem(title: "Sunset Time \(sunset_time)",
+                                action: nil,
+                                keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Check Weather",
+                                action: #selector(AppDelegate.updateWeather),
+                                keyEquivalent: "U"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "About",
+                                action: #selector(AppDelegate.about),
+                                keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit",
+                                action: #selector(NSApplication.terminate),
+                                keyEquivalent: "Q"))
+        statusItem.menu = menu
     }
     
-    @objc func getWeather(_ sender: Any?) {
+    @objc func about() {
+        
+    }
+    
+    func withinNormalHours() -> Bool {
+        let now = Date()
+        let opening_time = now.dateAt(hours: 13, minutes: 0)
+        let closing_time = now.dateAt(hours: 21, minutes: 0)
+        return (now >= opening_time && now <= closing_time)
+    }
+    
+    @objc func updateWeather() {
         if (withinNormalHours()) {
-            updateWeather()
+            fetchWeather()
         } else {
             setStatusClosed()
         }
@@ -72,12 +116,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             button.image = NSImage(named:NSImage.Name("RedFlag"))
         }
     }
-    @objc func updateWeather() {
+    
+    @objc func fetchWeather() {
         guard let the_url = URL(string: ENDPOINT) else {
             print("ERROR: \(ENDPOINT) is not a valid URL")
             return
         }
-        
+        print("Fetching flag status...")
         do {
             let html = try String(contentsOf: the_url, encoding: .ascii)
             switch html {
@@ -93,16 +138,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         } catch let error {
             print("Error: \(error)")
         }
-        let weatherText = "Weather!"
-        print("\(weatherText)")
-    }
-    
-    func constructMenu() {
-        let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "Check Weather", action: #selector(AppDelegate.updateWeather), keyEquivalent: "U"))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate), keyEquivalent: "Q"))
-        statusItem.menu = menu
     }
 }
 
